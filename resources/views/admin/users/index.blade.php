41bbfb45e0c5d56c81a72e099091ffc922b28648
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-09-06 18:38:22 -0300

    feat: users view shows table with pagination

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,11 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Users</title>
 </head>
 
 <body>
-    <h1>Olá {{ $user->name }}</h1>
+    <h1>Usuários</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        {{-- <a href="{{ route('admin.users.edit', $user->id) }}">Editar</a> --}}
+                    </td>
+                </tr>
+            @empty
+            <tr>
+                <td colspan="100">Nenhum usuário no banco</td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{-- pagination --}}
+    {{ $users-> links() }}
 </body>
 
 </html>
