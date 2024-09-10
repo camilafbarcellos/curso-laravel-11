9df64518bf821ef70902e7f0a4f5d1a9933fe5ea
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-09-10 16:15:35 -0300

    refactor: refactor forms with partial

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,14 +5,8 @@
 @section('content')
     <h1>Editar o Usuário {{ $user->name }}</h1>
 
-    <x-alert/>
-
     <form action="{{ route('users.update', $user->id) }}" method="POST">
-        @csrf() {{-- sends the csrf token hidden --}}
         @method('PUT') {{-- submits with put method --}}
-        <input type="text" name="name" placeholder="Nome" value="{{ $user->name }}">
-        <input type="email" name="email" placeholder="E-mail" value="{{ $user->email }}">
-        <input type="password" name="password" placeholder="Senha">
-        <button type="submit">Enviar</button>
+        @include('admin.users.partials.form')
     </form>
 @endsection
