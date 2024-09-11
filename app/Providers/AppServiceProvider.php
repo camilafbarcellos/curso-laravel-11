73105858d28c8188dc37dff6bb44426c2ca5f057
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-09-11 17:42:38 -0300

    feat: add admin and owner gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // admin authentication gate
+        Gate::define('is-admin', function (User $user): bool {
+            return $user->isAdmin();
+        });
+
+        // owner verification gate
+        Gate::define('owner', function (User $user, object $register): bool {
+            return $user->id === $register->user_id;
+        });
     }
 }
